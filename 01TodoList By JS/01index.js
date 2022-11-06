e686ba83440a74ea69c564c963edf0e81b6d502f
star <2654109566@qq.com>
2022-11-06 13:39:13 +0800

    some fatal html and css

---
+++ 01TodoList By JS/01index.js
@@ -0,0 +1,5 @@
+const todoList = document.getElementById('todoList')
+const doingList = document.getElementById('doingList')
+const doneList = document.getElementById('doneList')
+let task = document.createElement('li')
+
